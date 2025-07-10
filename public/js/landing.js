daefd9d3580ae5c84186787ded36b1e5a3d1ff2f
SKD <skd1545passion@gmail.com>
2025-07-10 22:03:56 +0530

    signup form

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,7 +1,20 @@
-// JS for landing page interactivity
 window.onload = () => {
     if (window.location.hash) {
         history.replaceState(null, null, ' ');
         window.scrollTo(0, 0);
     }
 };
+
+const signupBtn = document.getElementById('signup');
+const signupOverlay = document.querySelector('.signup');
+const signupBox = signupOverlay.querySelector('.signup-container');
+
+signupBtn.addEventListener('click', () => {
+    signupOverlay.style.display = 'flex';  
+});
+
+signupOverlay.addEventListener('click', (evt) => {
+    if (!signupBox.contains(evt.target)) {
+        signupOverlay.style.display = 'none';
+    }
+});
