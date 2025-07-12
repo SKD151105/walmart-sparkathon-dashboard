3f34def28a2768b14162eb5c8e7e337b400e0770
AjinkyaJoshi05 <aaj.ajinkya@gmail.com>
2025-07-12 18:54:02 +0530

    dependencies update 1

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
+import env from "dotenv";
 
 
 // const express = require('express');
